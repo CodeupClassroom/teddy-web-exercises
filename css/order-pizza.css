f19508fd8263d2ba892f328ee21de4da378a8698
Ryan Orsinger <ryan.orsinger@gmail.com>
2018-01-19 17:00:35 -0600

    Add order-pizza.html exercise

---
+++ css/order-pizza.css
@@ -0,0 +1,10 @@
+.jumbotron {
+    background-image: url("../img/pizza.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.jumbotron > h1 {
+    background-color: rgba(169,169,169, 0.3);
+    text-align: center;
+}

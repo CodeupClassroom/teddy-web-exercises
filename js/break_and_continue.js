f1d200496f8f54019cea57735829e3466044f986
Ryan Orsinger <ryan.orsinger@gmail.com>
2018-01-30 16:04:57 -0600

    Add loop solutions

---
+++ js/break_and_continue.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// Prompt the user for an odd number between 1 and 50.
+var input;
+
+do {
+    input = prompt("Please input a number between 1 and 50");
+} while(input > 50 || input < 1 || isNaN(parseFloat(input)));
+
+console.log("number to skip is " + input);
+
+// Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+for(var i = 1; i <= 50; i++) {
+    if(i % 2 == 0) {
+        continue;
+    }
+    if(i == input) {
+        console.log("Yikes! Skipping the number " + i);
+        continue;
+    }
+
+    console.log("Here is an odd number " + i);
+
+}

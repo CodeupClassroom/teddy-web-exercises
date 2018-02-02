05dd84ebba5c42618d70b3317fbdef7ffeef0008
Justin Reich <justinreich.dev@gmail.com>
2018-02-02 11:55:14 -0600

    Add solution to math object lesson

---
+++ js/circle.js
@@ -0,0 +1,48 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+
+            var area = Math.PI * Math.pow(this.radius, 2);
+
+            return area; // TODO: return the proper value
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+            var result = this.getArea();
+            if (doRounding) {
+                result = Math.round(this.getArea());
+            }
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + result);
+        }
+    };
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+
+    circle.radius = 5;
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+})();

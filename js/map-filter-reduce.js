40ad7000ff4c7ca2e3c382150b9826c42a7dcea2
Justin Reich <justinreich.dev@gmail.com>
2018-02-21 09:49:58 -0600

    Starting code for exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+
+// PROBLEM 1
+// Create a file named map-filter-reduce.js in your
+// js directory and copy the data below into it.
+
+
+
+// PROBLEM 2
+// Use .filter to create an array of user objects
+// where each user object has at least 3 languages in the languages array.
+
+
+// PROBLEM 3
+// Use .map to create an array of strings where each
+// element is a user's email address
+
+
+// PROBLEM 4
+// Use .reduce to transform the array into an object
+// where the object's keys are ids and the values are objects that represent each user
+
+
